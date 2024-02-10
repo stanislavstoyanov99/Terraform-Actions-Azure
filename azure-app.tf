36e8e517854edf99d91c9f4cee59e25fb696cbca
Stanislav Stoyanov <45464117+stanislavstoyanov99@users.noreply.github.com>
2024-02-10 23:11:37 +0200

    Update azure-app.tf

--- azure-app.tf
+++ azure-app.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragestan"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
